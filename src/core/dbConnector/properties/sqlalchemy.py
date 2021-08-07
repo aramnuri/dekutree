32d63f8496828aefc2e214c99996739a40c73bac
bludcode <bludcode@naver.com>
2021-08-07 18:08:50 +0900

    create base project structure

---
+++ src/core/dbConnector/properties/sqlalchemy.py
@@ -0,0 +1,14 @@
+arguments = {
+    'echo' : False,
+    'echo_pool' : False,
+    'encoding' : 'utf-8',
+    'max_overflow' : 10,
+    'paramstyle' : None,
+    'pool' : None,
+    'poolclass' : None,
+    'pool_size' : 5,
+    'pool_recycle' : -1,
+    'pool_reset_on_return' : 'rollback',
+    'pool_timeout' : 30,
+    'pool_use_lifo' : False
+}
